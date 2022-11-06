88fa5f9969b1bb9b1a3040cd9e4cbaeff1b59bb0
okhromenko <veronika35456@gmail.com>
2022-11-06 23:52:47 +1000

    Lesson 2: add info page

--- merch_shop/database/seeders/PageSeeder.php
+++ merch_shop/database/seeders/PageSeeder.php
@@ -1,22 +1,20 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Page;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class PageSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call([
-            PageSeeder::class,
-        ]);
+        Page::query()->delete();
+        Page::factory(random_int(20, 30))->create();
     }
 }
